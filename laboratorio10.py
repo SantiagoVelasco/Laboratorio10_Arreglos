7d9ae39cf2526855452fcc6283836491bf261d75
David Velasco Daza <david.velasco.2020@upb.edu.co>
2020-04-09 22:47:04 -0500

    Modificación con múltiples a de entrada

--- laboratorio10.py
+++ laboratorio10.py
@@ -10,12 +10,18 @@ a = int(input('Ingrese el valor a: '))
 b = int(input('Ingrese el valor b: '))
 
 # Se crea la función para calcular el valor a elevado en el valor b.
+# Si a = 0 se termina el ciclo y la función.
 def a_power_b(a, b):
-    acu = 1
-    for i in range (0, b, 1):
-        potencia = acu*a
-        acu = potencia
-    print('El resultado del valor a, elevado a el valor b es: ' + str(acu))
-
+    while a != 0:
+        acu = 1
+        for i in range (0, b, 1):
+            potencia = acu*a
+            acu = potencia
+        print('El resultado del valor a, elevado a el valor b es: ' + str(acu))
+        a = int(input('Ingrese el valor a: '))
+        b = int(input('Ingrese el valor b: '))
+    else:
+        print('El valor a es 0.')
+        
 # Se ejecuta la función anteriormente creada.
 a_power_b(a, b)
